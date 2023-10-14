cad915182b089ae9c4ca16d9fef5025ace68ee58
AnuruddhaRaptor <anuruddhagawai@gmail.com>
2023-10-14 12:55:34 +0530

    Conditional Statements in Dart - 03

---
+++ Lectures/14/p11.dart
@@ -0,0 +1,30 @@
+
+/* You know java python doesn't support float value in switch,
+but in dart it works as butter smooth.
+
+*/ 
+
+
+void main() {
+
+        var x = 1.0;
+
+        switch(x) {
+
+                case 1.0 :
+                        print("One");
+
+                case 2.0 :
+                        print("Two");
+
+                case 3.0 :
+                        print("Three");
+
+                case 4.0 :
+                        print("Four");
+
+                default :
+                        print("No Match");
+        }
+}
+
---
+++ Lectures/14/p12.dart
@@ -0,0 +1,25 @@
+
+
+void main() {
+
+        var x = 1.0;
+
+        switch(x) {
+
+                case 1.0 :
+                        print("One");
+
+                case 2.0 :
+                        print("Two");
+
+                case 1.0 :
+                        print("Three");
+
+                case 1.0 :
+                        print("Four");
+
+                default :
+                        print("No Match");
+        }
+}
+
