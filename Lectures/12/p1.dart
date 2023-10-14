affa7e220675bc3e73ace19b2e80b89fc5dd3106
AnuruddhaRaptor <anuruddhagawai@gmail.com>
2023-10-14 12:54:26 +0530

    Conditional Statements in Dart - 01

---
+++ Lectures/12/p1.dart
@@ -0,0 +1,22 @@
+
+
+// Lecture : 12 
+
+/*
+	Topic : Control Statements 
+	Date : 22-August-2023 : Tuesday
+	Lecture Started On : 7:10 AM
+*/
+
+// If Statement
+
+void main() {
+	
+	int x = 7;
+	print(x);
+
+	if(x < 10) {
+		print("x less than 10");
+	}
+	print("End main");
+}
